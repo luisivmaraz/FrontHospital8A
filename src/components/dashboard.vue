982381bffddadd8b086cead20a1154dd812fc7e1
Aldotd12 <tolentinodomingodiego@gmail.com>
2025-03-07 20:09:07 -0600

    Añadi la sección de RECURSOS MATERIALES

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -235,7 +235,6 @@
 
 
             <!-- RECURSOS MATERIALES -->
-            <!-- Recursos Materiales -->
             <li>
                <button type="button"
                   class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
